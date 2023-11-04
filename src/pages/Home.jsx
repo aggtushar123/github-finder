39d0cfd19a283e35de922c93ad48f45c1360b58a
Tushar Aggarwal <aggtushar123@gmail.com>
2023-11-05 02:25:23 +0530

    Github Context using useContext

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserResults from '../components/users/UserResults'
+
+function Home() {
+  return (
+    <div>
+
+        
+        <UserResults/>  
+    </div>
+  )
+}
+
+export default Home
